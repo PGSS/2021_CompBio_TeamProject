306553415e7613476c116d4fc4324ae3f38d0745
Alejandro Lucena <alelucena0836@gmail.com>
2021-07-19 17:03:11 -0400

    Basic sequence loading.

--- virtual_digest/database_loader.py
+++ virtual_digest/database_loader.py
@@ -4,5 +4,7 @@ from os.path import expanduser
 
 home = expanduser("~")
 
-for record in SeqIO.parse(home + "/current_Bacteria_unaligned.fa", "fasta"):
-    print(record.id)
+for seq_record in SeqIO.parse(home + "/current_Bacteria_unaligned.fa", "fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
