736a28e406808a82acacdc11272cb4684b5a4aba
Hannah Chang <hannahch@andrew.cmu.edu>
2021-07-29 15:57:54 -0400

    sample virtual digest working!

---
+++ virtual_digest/possibility reducer.py
@@ -0,0 +1,26 @@
+
+# input: bacteria name, list of fragments
+
+# list of seq object (From digest)
+    # name, ascession (ID) #, dictionary (name of enzyme as key, fragment sizes)
+
+# list of seq object (without name)
+    # name, ascession (ID) #, dictionary (name of enzyme as key, fragment sizes)
+
+def pos_reducer(seq_name, seq):
+
+    # compare the two lists of objects
+        # compare fragment sizes, and match the correspoding name
+        # if none are matched then return null
+
+    for i in range (0, len(seq)-1) :
+
+        #if (seq[i].dict
+
+    # taking in fragment sizes for each enzyme (3)
+    # do the match, name with fragments
+    # comparing the no name to name
+
+    # output :
+        # list of name and fragments
+        # dictionary
